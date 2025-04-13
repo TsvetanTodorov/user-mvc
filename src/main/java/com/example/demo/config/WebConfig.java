c760193c3334bf8860bc3f6fbc210127d68c0018
TsvetanTodorov <tstodorofff@gmail.com>
2025-04-13 23:30:30 +0300

    Refactoring Web Config

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -4,15 +4,16 @@ import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.filter.HiddenHttpMethodFilter;
+
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
@@ -36,6 +37,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(form -> form
                         .loginPage("/auth/login")
+                        .loginProcessingUrl("/login")
                         .usernameParameter("username")
                         .passwordParameter("password")
                         .permitAll()
@@ -47,5 +49,15 @@ public class WebConfig implements WebMvcConfigurer {
         return http.build();
     }
 
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        UserDetails userDetails = User.withUsername("user")
+                .password(passwordEncoder().encode("123123"))
+                .build();
+
+        return new InMemoryUserDetailsManager(userDetails);
+    }
 
 }
